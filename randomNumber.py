1f2e253544b10ca247ff664ff2ca25c794492099
zhengyugu <a1021604754@gmail.com>
2019-01-30 10:19:10 -0600

    add more case

--- randomNumber.py
+++ randomNumber.py
@@ -1,12 +1,18 @@
-enumerate([4,3,2,1])
-for i,j in enumerate([4,3,2,1]):
-  print (i)
+import random
 
-hashl={}
-hashl[0] =10
-hashl[1] =11
-print(hashl)
-if hashl[0]:
-  print("aaaaa")
-if 2 in hashl:
-  print("asdsd")
+for i in range(0,9):
+  print(random.randint(0, 10))
+
+
+# enumerate([4,3,2,1])
+# for i,j in enumerate([4,3,2,1]):
+#   print (i)
+#
+# hashl={}
+# hashl[0] =10
+# hashl[1] =11
+# print(hashl)
+# if hashl[0]:
+#   print("aaaaa")
+# if 2 in hashl:
+#   print("asdsd")
